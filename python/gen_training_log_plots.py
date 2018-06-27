de7e389ecc5d9ebd6e7d05308a6b261054b39e6b
Ren Zhang <ren.zhang@ibm.com>
2018-06-27 13:15:29 -0400

    minor fix, update progress plots

--- python/gen_training_log_plots.py
+++ python/gen_training_log_plots.py
@@ -14,7 +14,7 @@ def plot_training_progress(model_name):
     fitting_times['fitting_time'] = fitting_times['fitting_time'].\
         map(lambda x: pd.Timedelta(x).total_seconds() / 60)
     fig = plt.figure()
-    plt.rcParams['figure.figsize'] = [16, 12]
+    plt.rcParams['figure.figsize'] = [16, 9]
     plt.rcParams['legend.fontsize'] = 20
     mean_loss, mean_acc = loss_progress.groupby('fold').agg({
         'valid_loss': lambda x: x.mean(),
@@ -44,24 +44,31 @@ def plot_training_progress(model_name):
 
     for fold_num in range(1,6):
         fold_data = loss_progress.loc[loss_progress['fold'] == fold_num]
-        ax1.plot(fold_data.epoch, fold_data.train_loss, color='b', alpha=0.4)
-        ax1.plot(fold_data.epoch, fold_data.valid_loss, color='r', alpha=0.4)
-        ax2.plot(fold_data.epoch, fold_data.train_acc, color='b', alpha=0.4)
-        ax2.plot(fold_data.epoch, fold_data.valid_acc, color='r', alpha=0.4)
+        ax1.plot(fold_data.epoch, fold_data.train_loss, color='b', alpha=0.3)
+        ax1.plot(fold_data.epoch, fold_data.valid_loss, color='r', alpha=0.3)
+        ax2.plot(fold_data.epoch, fold_data.train_acc, color='b', alpha=0.3)
+        ax2.plot(fold_data.epoch, fold_data.valid_acc, color='r', alpha=0.3)
+        for lr_decay_loc in lr_decay.loc[lr_decay.fold == fold_num].epoch:
+            ax1.axvline(x=lr_decay_loc, color='m', alpha=0.1, linestyle=':')
+            ax2.axvline(x=lr_decay_loc, color='m', alpha=0.1, linestyle=':')
         if fold_num == 1:
             ax1.legend(loc='upper right', ncol=2, frameon=True)
             ax2.legend(loc='lower right', ncol=2, frameon=True)
     for lr_decay_loc in lr_decay.groupby(['lr']).mean().epoch.values:
-        ax1.axvline(x=lr_decay_loc, color='m', alpha=0.8, linestyle=':')
-        ax2.axvline(x=lr_decay_loc, color='m', alpha=0.8, linestyle=':')
-    ax1.axvline(x=terminate_loss_loc, color='y', alpha=0.8, linestyle='--')
-    ax2.axvline(x=terminate_acc_loc, color='y', alpha=0.8, linestyle='--')
+        ax1.axvline(x=lr_decay_loc, color='m', alpha=0.8, linestyle='-')
+        ax2.axvline(x=lr_decay_loc, color='m', alpha=0.8, linestyle='-')
+
+    ax1.axvline(x=terminate_loss_loc, color='y', alpha=0.8, linestyle='-')
+    ax2.axvline(x=terminate_acc_loc, color='y', alpha=0.8, linestyle='-')
     # plt.show()
     plt.savefig('../Presentation/plot_training_progress_{}.png'.format(model_name))
     plt.close('all')
 
 files = os.listdir('../Presentation/')
 model_names = ['_'.join(filename.split('.md')[0].split()) for filename in files if filename.endswith('.md')]
+
+plt.rcParams['figure.figsize'] = [16, 9]
+plt.rcParams['legend.fontsize'] = 20
 
 for model_name in model_names:
     print(' ---- plotting model {}'.format(model_name))
