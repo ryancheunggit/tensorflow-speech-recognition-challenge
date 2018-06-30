5530bb8943858b9f7ea8657446c9313e36ff98b0
Ren Zhang <ren.zhang@ibm.com>
2018-06-30 16:36:00 -0400

    fix the std values on plot

--- python/gen_training_log_plots.py
+++ python/gen_training_log_plots.py
@@ -5,13 +5,14 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 plt.style.use('fivethirtyeight')
 
+
 def plot_training_progress(model_name):
     # read in parsed log data
     loss_progress = pd.read_csv('../Presentation/loss_progress_{}.csv'.format(model_name))
     lr_decay = pd.read_csv('../Presentation/lr_decay_{}.csv'.format(model_name))
     fitting_times = pd.read_csv('../Presentation/fitting_times_{}.csv'.format(model_name))
     # convert fitting times to minutes
-    fitting_times['fitting_time'] = fitting_times['fitting_time'].\
+    fitting_times['fitting_time'] = fitting_times['fitting_time']. \
         map(lambda x: pd.Timedelta(x).total_seconds() / 60)
     fig = plt.figure()
     plt.rcParams['figure.figsize'] = [16, 9]
@@ -21,8 +22,8 @@ def plot_training_progress(model_name):
         'valid_acc': lambda x: x.mean()
     }, axis=0).mean()
     std_loss, std_acc = loss_progress.groupby('fold').agg({
-        'valid_loss': lambda x: x.mean(),
-        'valid_acc': lambda x: x.mean()
+        'valid_loss': lambda x: x.std(),
+        'valid_acc': lambda x: x.std()
     }, axis=0).mean()
     plt.suptitle(model_name.replace('arm', '').replace('_', ' ').replace('ddnn', 'fc') + \
                  '   ({0:.2f} ± {1:.2f} minutes)'.format(
@@ -30,19 +31,19 @@ def plot_training_progress(model_name):
                      fitting_times['fitting_time'].std(),
                  ) + '\n' + \
                  ' Logloss: {0:.2f} ± {1:.2f}  -- Accuracy:  {2:.2f} ± {3:.2f}'.format(
-                 mean_loss, std_loss, mean_acc, std_acc
+                     mean_loss, std_loss, mean_acc, std_acc
                  )
-                 , fontsize=24, linespacing=1.5)
+                 , fontsize=18, linespacing=1.2)
     ax1 = plt.subplot(211)
     plt.title('Loss by epochs')
     ax2 = plt.subplot(212)
     plt.title('Accuracy by epochs')
     terminate_loss_loc, terminate_acc_loc = loss_progress.groupby('fold').agg({
-            'valid_loss': lambda x: x.values.argmin(),
-            'valid_acc': lambda x: x.values.argmax()
-        }, axis=0).mean()
+        'valid_loss': lambda x: x.values.argmin(),
+        'valid_acc': lambda x: x.values.argmax()
+    }, axis=0).mean()
 
-    for fold_num in range(1,6):
+    for fold_num in range(1, 6):
         fold_data = loss_progress.loc[loss_progress['fold'] == fold_num]
         ax1.plot(fold_data.epoch, fold_data.train_loss, color='b', alpha=0.3)
         ax1.plot(fold_data.epoch, fold_data.valid_loss, color='r', alpha=0.3)
